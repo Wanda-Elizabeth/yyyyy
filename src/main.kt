39676f7b093db1b4dec88356f15a2a42fc9ab8a7
WandaElizabeth <wandawairimu@gmail.com>
2022-03-10 08:58:58 +0300

    assesment

---
+++ src/main.kt
@@ -0,0 +1,36 @@
+fun main(){
+
+    var name="Codehive"
+    var y=name[5].toString()+name[6]+name[7]+name[8]
+    println(y)
+    modulus(13,56)
+    var c=fact("Wanda",22)
+    print(c)
+    var k=("Khloe")
+    println(k.count())
+
+}
+fun modulus(num:Int,num2:Int){
+    var mod=num%num2
+   return (modulus)
+
+}
+
+fun fact(name:String,age:Int):String{
+    var c=("Hi,my name is $name  and i am  $age  years old")
+    return c
+
+}
+fun detail(name: String):String{
+    var name="Khloe"
+    return name
+}
+
+
+
+
+
+
+
+
+
